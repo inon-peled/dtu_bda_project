e3d833af70c2a4f51c1490110da5a3815ae9a83f
inonpeled <inon.peled@gmail.com>
2018-04-15 13:23:45 +0200

    HA

---
+++ plot_quarterly.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+def plot_quarterly(model_name, mean_predictions, ci_95_half_widths, y_test_2014):
+    def add_date_index(values):
+        return pd.Series(values, 
+                         index=pd.date_range(start=datetime(2014, 1, 1), end=datetime(2014, 12, 30)))
+    
+    def filter_date_range(series):
+        return series\
+            [lambda df: df.index >= from_date_inclusive]\
+            [lambda df: df.index < to_date_non_inclusive]
+    
+    plt.tight_layout()
+    for i, (quarter, from_date_inclusive, to_date_non_inclusive) in enumerate([
+        ('Q1', datetime(2014, 1, 1), datetime(2014, 4, 1)),
+        ('Q2', datetime(2014, 4, 1), datetime(2014, 7, 1)),
+        ('Q3', datetime(2014, 7, 1), datetime(2014, 10, 1)),
+        ('Q4', datetime(2014, 10, 1), datetime(2014, 12, 31))
+    ]):    
+        plt.subplot(411 + i)
+        plt.plot(filter_date_range(y_test_2014), "b-", label='Actual')
+        plt.plot(filter_date_range(mean_predictions), "r", label='Predicted')
+        plt.fill_between(filter_date_range(y_test_2014).index, 
+                         filter_date_range(mean_predictions + ci_95_half_widths),
+                         filter_date_range(mean_predictions - ci_95_half_widths),
+                         color='lightgrey', label='95\% C.I.')
+        plt.legend(loc='lower right', fontsize=12)
+        if i == 0:
+            plt.title(model_name, fontsize=20)
